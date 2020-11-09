3f0cd5b28bb6e6105881b79fb6c73385ee6bc6f6
AlbertoRRM <argalad@tutamail.com>
2020-11-09 14:54:16 +0100

    Añadidos algunos elementos más de la conexión

--- P1_sockets/serverGame.c
+++ P1_sockets/serverGame.c
@@ -1,49 +1,50 @@
 #include "serverGame.h"
 #include <pthread.h>
 
-void showError(const char *msg)
+void showError (const char *msg)
 {
-  perror(msg);
-  exit(0);
+  perror (msg);
+  exit (0);
 }
 
-void *threadProcessing(void *threadArgs)
-{
-  tSession session;				         /** Session of this game */
-  int socketPlayer1;				       /** Socket descriptor for player 1 */
-  int socketPlayer2;				       /** Socket descriptor for player 2 */
-  tPlayer currentPlayer;			     /** Current player */
-  int endOfGame;				           /** Flag to control the end of the game*/
-  unsigned int card;				       /** Current card */
-  unsigned int code, codeRival;	   /** Codes for the active player and the rival */
+/*Descomentar para implementación de threads*/
+// void *threadProcessing(void *threadArgs)
+// {
+//   tSession session;				         /** Session of this game */
+//   int socketPlayer1;				         /** Socket descriptor for player 1 */
+//   int socketPlayer2;				         /** Socket descriptor for player 2 */
+//   tPlayer currentPlayer;			       /** Current player */
+//   int endOfGame;				             /** Flag to control the end of the game*/
+//   unsigned int card;				         /** Current card */
+//   unsigned int code, codeRival;	   /** Codes for the active player and the rival */
 
-  // Get sockets for players
-  socketPlayer1 = ((tThreadArgs *) threadArgs)->socketPlayer1;
-  socketPlayer2 = ((tThreadArgs *) threadArgs)->socketPlayer2;
+//   // Get sockets for players
+//   socketPlayer1 = ((tThreadArgs *) threadArgs)->socketPlayer1;
+//   socketPlayer2 = ((tThreadArgs *) threadArgs)->socketPlayer2;
 
-  // Receive player 1 info
-
-
-  // Receive player 2 info
+//   // Receive player 1 info
 
 
-  // Init...
-  endOfGame = FALSE;
-
-  while (!endOfGame)
-  {
+//   // Receive player 2 info
 
 
-  }
+//   // Init...
+//   endOfGame = FALSE;
 
-  // Close sockets
-  close (socketPlayer1);
-  close (socketPlayer2);
+//   while (!endOfGame)
+//   {
 
-  return (NULL) ;
-}
 
-int main(int argc, char *argv[])
+//   }
+
+//   // Close sockets
+//   close (socketPlayer1);
+//   close (socketPlayer2);
+
+//   return (NULL) ;
+// }
+
+int main (int argc, char *argv[])
 {
   int socketfd;				                    /** Socket descriptor */
   struct sockaddr_in serverAddress;	      /** Server address structure */
@@ -55,44 +56,85 @@ int main(int argc, char *argv[])
   unsigned int clientLength;		          /** Length of client structure */
   tThreadArgs *threadArgs; 		            /** Thread parameters */
   pthread_t threadID;			                /** Thread ID */
+  tString message;
+  int msgLength;
 
   // Seed
-  srand(time(0));
+  srand (time (0));
 
   // Check arguments
   if (argc != 2)
   {
-    fprintf(stderr,"ERROR wrong number of arguments\n");
-    fprintf(stderr,"Usage:\n$>%s port\n", argv[0]);
+    fprintf (stderr,"ERROR wrong number of arguments\n");
+    fprintf (stderr,"Usage:\n$>%s port\n", argv[0]);
     exit(1);
   }
 
   // Create the socket
-  socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  socketfd = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (socketfd < 0)
-    showError("Error while creating the socket");
+    showError ("Error while creating the socket");
 
   // Init server structure
-  memset(&serverAddress, 0, sizeof(serverAddress));
+  memset (&serverAddress, 0, sizeof(serverAddress));
 
   // Get listening port
-  port = atoi(argv[1]);
+  port = atoi (argv[1]);
 
   // Fill server structure
   serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-  serverAddress.sin_port = htons(port);
+  serverAddress.sin_addr.s_addr = htonl (INADDR_ANY);
+  serverAddress.sin_port = htons (port);
 
   // Bind
-  if (bind(socketfd,
+  if (bind (socketfd,
     (struct sockaddr *) &serverAddress,
-    sizeof(serverAddress)) < 0)
-    showError("ERROR while binding");
+    sizeof (serverAddress)) < 0)
+    showError ("ERROR while binding");
 
   // Listen
-  listen(socketfd, 2);
+  listen (socketfd, 2);
 
+  // Get length of Player 1 structure
+  clientLength = sizeof (player1Address);
+
+  // Accept
+  socketPlayer1 = accept (socketfd, (struct sockaddr *) &player1Address, &clientLength);
+  if (socketPlayer1 < 0)
+    showError ("ERROR while accepting player 1");
+  else
+    fprintf(stdout, "Player 1 is connected!\n");
+
+  // Get length of Player 2 structure
+  clientLength = sizeof (player2Address);
+
+  // Accept
+  socketPlayer2 = accept (socketfd, (struct sockaddr *) &player2Address, &clientLength);
+  if (socketPlayer2 < 0)
+    showError ("ERROR while accepting player 2");
+  else
+    fprintf (stdout, "Player 2 is connected!\n");
+
+  // Init and read Player 1 name
+  memset (message, 0, STRING_LENGTH);
+  msgLength = recv (socketPlayer1, message, STRING_LENGTH - 1, 0);
   
+  // Check read bytes
+  if (msgLength < 0)
+    showError ("ERROR while reading name of player 1");
+  else
+    printf ("Name of player 1 received: %s\n", message);
+
+  // Init and read Player 2 name
+  memset (message, 0, STRING_LENGTH);
+  msgLength = recv (socketPlayer2, message, STRING_LENGTH - 1, 0);
+  
+  // Check read bytes
+  if (msgLength < 0)
+    showError ("ERROR while reading name of player 2");
+  else
+    printf ("Name of player 2 received: %s\n", message);
+
   // Infinite loop
   while (1)
   {
